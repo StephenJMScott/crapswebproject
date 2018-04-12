c38c17eafb2c31d62e29d91cdb28ee789953ef72
Stephen Scott <stephenjmscott@gmail.com>
2018-04-12 10:42:16 +0000

    dice display and score display added 11.38 12/04/2018

--- js/app.js
+++ js/app.js
@@ -25,3 +25,28 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+//JavaScript for rolling the dice
+
+
+document.querySelector(".rollTheDice").addEventListener("click",function(){
+    
+    //1. get two random numbers
+    var die1 = Math.floor(Math.random()*6) + 1;
+    var die2 = Math.floor(Math.random()*6) + 1;
+    
+    //2. update dice image to reflect random number
+    
+    var die1DOM = document.getElementById("left-dice");
+    die1DOM.src = "css/assets/dice-" + die1 + ".png";
+    
+    var die2DOM = document.getElementById("right-dice");
+    die2DOM.src = "css/assets/dice-" + die2 + ".png";
+    
+    //3. add the dice and display the score
+    
+    var score = die1 + die2;
+    document.getElementById("messagedDisplayed").innerHTML = "You rolled a " + score + "!"  ;
+    
+});
